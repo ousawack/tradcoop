2af9836de452bf6e56bee45ad55cdddb8a4d277a
ousama <ousawack@gmail.com>
2022-09-11 21:26:01 +0100

    created a splash screen
    implemented a splash screen with application logo in it

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const SplashScreen = () => {
+const HomeScreen = () => {
   return (
     <View>
-      <Text>SplashScreen</Text>
+      <Text>HomeScreen</Text>
     </View>
   );
 };
 
-export default SplashScreen;
+export default HomeScreen;
