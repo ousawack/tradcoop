b2e0a11c3f6767d53a3530b5c375be72c5a97fe9
lubu-22 <hamza22zirar@gmail.com>
2022-09-15 18:56:43 +0200

    Cards Components
    added cards for each row

---
+++ components/ProductCard.jsx
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { MapPinIcon, StarIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
+
+/* Coop Product Card */
+
+const ProductCard = ({ imgUrl, title, rating, genre, address }) => {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2 text-[#4E1703]">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="#C3700D" opacity={0.5} size={22} />
+          <Text className="text-[#7B420E]">
+            <Text className="text-[#C3700D]">{rating}</Text> . {genre}
+          </Text>
+        </View>
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="#7B420E" opacity={0.4} size={22} />
+          <Text className="text-xs text-[#7B420E]">Nearby . {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ProductCard;
