e0d59d77a641619a11894dcfe09a9124c280aa9e
lubu-22 <hamza22zirar@gmail.com>
2022-09-15 18:59:10 +0200

    Navigation routes
    Navigation routes  for each screen

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
 
+import HomeScreen from "./screens/HomeScreen";
+import CoopScreen from "./screens/CoopScreen";
+import CoopListScreen from "./screens/CoopListScreen";
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Coop"
+          component={CoopScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CoopList"
+          component={CoopListScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
