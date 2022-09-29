47ac0710b7b37aebb6ecb8f880268cfe2a928470
ousama <ousawack@gmail.com>
2022-09-29 13:29:29 +0100

    added product schema

---
+++ tradcoop-sanity/schemas/product.js
@@ -0,0 +1,39 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name of product",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "short_description",
+      title: "Short description",
+      type: "string",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: "price",
+      title: "Price of the product in MAD",
+      type: "number",
+    },
+    {
+      name: "image",
+      title: "Image of the Product",
+      type: "image",
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a Rating from (1-5 stars)",
+      validation: (Rule) =>
+        Rule.required()
+          .min(1)
+          .max(5)
+          .error("Please enter a Value between 1 and 5"),
+    },
+  ],
+};
